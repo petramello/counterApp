b3facf948bf403c500e763de868c2e940f2968ab
petramello <petramello@gmail.com>
2021-05-19 11:11:06 -0300

    Counter: turning it functional

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
+
+
+const getStyle = backgroundColor => {
+  return {
+    backgroundColor,
+    padding: '10px',
+    margin: '5px',
+    borderRadius: '5px',
+  }
+}
 
 const Counter = (props) => {
 
-  const getStyle = backgroundColor => {
-    return {
-      backgroundColor,
-      padding: '10px',
-      margin: '5px',
-      borderRadius: '5px'
+  const [counter, setCounter] = useState(0);
+
+  const subtractCounter = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
     }
   }
 
+
   return (
-    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+    <div style={{display: 'flex', flexDirection: 'column', margin: '10px', alignItems: 'center', justifyContent: 'center'}}>
       <span>{props.product}</span>
       <div style={{display: 'flex', color: '#ffffff', alignItems: 'center', justifyContent: 'center' }}>
-        <div style={getStyle('#F6C006')}>Zero</div>
-        <div style={getStyle('#6C757D')}>+</div>
-        <div style={getStyle('#69C3D1')}>-</div>
-        <div style={getStyle('#DC3545')}><FaTrashAlt /></div>
+        <div style={{...getStyle('#F6C006'), cursor: 'initial'}}>
+          {counter || 'Zero'}
+        </div>
+        <div onClick={ () => setCounter(counter + 1)} style={{...getStyle('#3CB371'), cursor: 'pointer'}} >
+          +
+        </div>
+        <div onClick={subtractCounter} style={{...getStyle(counter>0 ? '#69C3D1' : 'grey'), cursor: counter>0 ? 'pointer' : 'initial'}} >
+          -
+        </div>
+        <div onClick={ () => setCounter(0)} style={{...getStyle('#DC3545'), cursor: counter>0 ? 'pointer' : 'initial'}} >
+          <FaTrashAlt />
+        </div>
       </div>
     </div>
 
